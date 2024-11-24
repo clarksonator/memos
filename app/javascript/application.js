d613d2bb7dae96fdae869b56e45e4d8a2fbf9d83
Nic Clarkson <clarksonator@gmail.com>
2024-11-24 11:54:49 -0700

    new delete confirmation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+Turbo.config.forms.confirm = function() {
+      let dialog = document.getElementById('turbo-confirm')
+      dialog.showModal();
+
+      return new Promise((resolve) => {
+            dialog.addEventListener("close", () => {
+                  resolve(dialog.returnValue == 'confirm')
+            }, { once: true })
+      })
+}
+
