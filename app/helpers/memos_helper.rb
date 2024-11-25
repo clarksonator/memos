3354c8f13c99d9e65152a86a86a22b3c4a9433b3
calvin <clarksonator@gmail.com>
2024-11-25 14:44:17 -0700

    edit memo

--- app/helpers/memos_helper.rb
+++ app/helpers/memos_helper.rb
@@ -1,14 +1,14 @@
 module MemosHelper
-  require 'redcarpet'
-  require 'rouge'
-  require 'rouge/plugins/redcarpet'
+  require "redcarpet"
+  require "rouge"
+  require "rouge/plugins/redcarpet"
+  require "markdown_checkboxes"
 
   class CustomRender < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
   end
 
-
-  @@markdown = Redcarpet::Markdown.new(CustomRender, fenced_code_blocks: true)
+  @@markdown = CheckboxMarkdown.new(CustomRender, fenced_code_blocks: true)
 
   def renderMarkdown(text)
     @@markdown.render(text)
